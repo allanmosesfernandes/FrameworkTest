6a88baa662db22ab4cee08abd6ca8637f84ae7b3
Allan Fernandes <Fernzallan14@gmail.com>
2023-08-28 12:57:28 +0100

    slider

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,42 +1,34 @@
 /* eslint-disable max-len */
 /* eslint-disable linebreak-style */
 /* eslint-disable indent */
+import { useState, useEffect } from "react";
 import Button from "./utils/Button";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+import BenefitsSlider from "./BenefitsSlider";
 
 function Benefits() {
-    const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-};
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
-return (
-  <section className="relative z-10 bg-mountains h-screen flex items-center flex-col">
-    <div className="absolute bg-blueGradient w-full h-screen" />
-    <div className="content mt-[10rem] text-center flex flex-col items-center justify-center z-50">
-      <p className="spacedText">The benefits of Space Solar</p>
-      <Slider {...settings}>
-        <div className="">
-          <div className="flex flex-col items-center justify-center w-6/12 m-auto">
-              <p className="subheading text-5xl leading-63 tracking-[-1px] my-4">
-                Convene an international partnership for development of space based energy.
-              </p>
-              <p className="bodycopy mb-20">
-                The UK has set out an ambitious national clean energy policy – Net Zero – to fully decarbonise the economy by 2050. This future energy scenario requires clean and sustainable energy generation from renewable sources.
-              </p>
-              <Button text="Business Plan" />
-          </div>
-        </div>
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
 
-      </Slider>
-    </div>
-  </section>
-);
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <section className="relative z-10 bg-mountains h-screen flex items-center flex-col">
+      <div className="absolute bg-blueGradient w-full h-screen items-center justify-center" />
+      <div className="content md:mt-[10rem] mt-[5rem] text-center flex flex-col z-50 w-full">
+        <p className="spacedText mb-4">{isMobile ? "Our Objectives" : "The benefits of Space Solar"}</p>
+        <BenefitsSlider />
+      </div>
+    </section>
+  );
 }
 
 export default Benefits;
--- src/components/Challenge.jsx
+++ src/components/Challenge.jsx
@@ -8,16 +8,15 @@ import ClimateChange from "../assets/images/climateChange.png";
 // eslint-disable-next-line prettier/prettier
 function Mission() {
 return (
-  <section className="bg-spaceBlue flex lg:flex-row flex-col-reverse font-sora justify-center lg:px-10  pt-[8.5rem] px-4 gap-6 overflow-hidden mb-20">
+  <section className="bg-spaceBlue flex lg:flex-row flex-col-reverse font-sora justify-center lg:px-10 pt-[8.5rem] px-4 gap-6 overflow-hidden mb-20">
     {/* Left */}
     <div className="lg:w-6/12 w-full flex flex-col mt-10">
       <p className="spacedText">
         The Global Challenge
       </p>
-      <p className="subheading text-5xl leading-63 my-4 tracking-[-1px]">
+      <p className="subheading lg:text-5xl md:text-4xl text-3xl lg:leading-63 md:leading-54 leading-42 my-4 tracking-[-1px]">
         Climate change and the pressures on global energy resources are urgent
-        problems. The UK has set out an ambitious national clean energy policy
-        to fully decarbonise the economy by 2050.
+        problems.
       </p>
       <p className="bodycopy mb-8">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna vitae elementum interdum scelerisque quisque donec nulla. Velit diam quam nullam elit hac sit. Nunc, lacus, facilisis mattis amet mauris volutpat dui tellus nam. Neque, rhoncus placerat vitae arcu habitant. Dignissim quam ipsum dolor egestas non orci. At enim ut elementum.
