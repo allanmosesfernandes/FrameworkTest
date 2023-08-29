ede89b3299cdce60cccc227fa504075219766b19
Allan Fernandes <Fernzallan14@gmail.com>
2023-08-29 21:59:41 +0100

    animation-on-button

--- src/components/utils/Arrow.jsx
+++ src/components/utils/Arrow.jsx
@@ -1,14 +1,14 @@
 import ArrowSVG from "../../assets/images/homepage/Arrow.svg";
 
 function Arrow(props) {
-  return (
-    <div
-      className="z-1 absolute bottom-10 left-[50%] w-[36px] h-[36px] border-white border-2 rounded-full flex justify-center items-center mx-auto"
-      onClick={props.onClick}
-    >
-      <img src={ArrowSVG} alt="Arrow" />
-    </div>
-  );
+	return (
+		<div
+			className="arrow-container spin z-1 absolute bottom-10 left-[50%] w-[36px] h-[36px] rounded-full flex justify-center items-center mx-auto"
+			onClick={props.onClick}
+		>
+			<img src={ArrowSVG} alt="Arrow" />
+		</div>
+	);
 }
 
 export default Arrow;
